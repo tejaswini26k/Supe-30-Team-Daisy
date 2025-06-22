c113f7d49f335e9f4c25510f352e6e5cd2dc5924
Tejaswini26 <tejaswinikolapkar25@gmail.com>
2025-06-22 18:25:37 +0530

    all

---
+++ e-commerce-per-app/e-commerce-per-app/server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const productsRoute = require('./routes/products');
+const customerRoute = require('./routes/customers');
+const ordersRoute = require('./routes/orders');
+const customerORoute = require('./routes/customers_orders');
+const authRoute = require('./routes/auth');
+const feedbackRoute = require('./routes/feedback');
+const statisticsRoutes = require('./routes/statistics'); 
+const overview = require('./routes/overview');
+const stores = require('./routes/stores');
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.use('/api/products', productsRoute);
+
+app.use('/api/customers', customerRoute);
+
+app.use('/api/orders', ordersRoute); 
+
+app.use('/api/customers_orders', customerORoute);
+
+app.use('/api/feedback', feedbackRoute);
+
+app.use('/api/auth', authRoute);
+
+app.use('/api/statistics', statisticsRoutes);
+
+app.use('/api/overview', overview);
+
+app.use('/api/stores' , stores);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
