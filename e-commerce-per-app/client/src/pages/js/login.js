d1b4993492ac0ca198483f99db02f2a7a5e53c2a
Tejaswini26 <tejaswinikolapkar25@gmail.com>
2025-07-22 18:48:42 +0530

    login

--- e-commerce-per-app/client/src/pages/js/login.js
+++ e-commerce-per-app/client/src/pages/js/login.js
@@ -23,7 +23,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await axios.post('/auth/login', {
+      const response = await axios.post('${process.env.REACT_APP_API_URL}/auth/login', {
         email,
         password,
         rememberMe
