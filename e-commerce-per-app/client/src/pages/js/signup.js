4062f0da9207a59488896678181b6d7978446a4f
Tejaswini26 <tejaswinikolapkar25@gmail.com>
2025-07-22 20:14:43 +0530

    sign

--- e-commerce-per-app/client/src/pages/js/signup.js
+++ e-commerce-per-app/client/src/pages/js/signup.js
@@ -35,7 +35,7 @@ const SignUp = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/signup', {
+      const response = await fetch('${process.env.REACT_APP_API_URL}/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
