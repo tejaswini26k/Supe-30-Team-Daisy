0f36398d47f9e9784dbed1078c06b427c6edaf1c
Tejaswini26 <tejaswinikolapkar25@gmail.com>
2025-07-22 18:36:49 +0530

    redo

--- e-commerce-per-app/client/src/pages/js/addCustomer.js
+++ e-commerce-per-app/client/src/pages/js/addCustomer.js
@@ -23,7 +23,7 @@ const AddCustomer = () => {
     try {
       const token = localStorage.getItem('authToken');
 
-      const response = await fetch('${process.env.REACT_APP_API_URL}/customers/add', {
+      const response = await fetch('http://localhost:5000/api/customers/add', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
