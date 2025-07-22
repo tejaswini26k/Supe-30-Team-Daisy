b2259ad018ba00d6816d5cd06a9b2d7c8969a608
Tejaswini26 <tejaswinikolapkar25@gmail.com>
2025-07-22 18:48:15 +0530

    dukaan

--- e-commerce-per-app/client/src/pages/js/orders.js
+++ e-commerce-per-app/client/src/pages/js/orders.js
@@ -25,7 +25,7 @@ const Orders = () => {
           return;
         }
 
-        const response = await axios.get('http://localhost:5000/api/orders', {
+        const response = await axios.get('${process.env.REACT_APP_API_URL}/api/orders', {
           headers: { Authorization: `Bearer ${token}` },
           params: { storeId }
         });
